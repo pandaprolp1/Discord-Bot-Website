5e1b1e11c46af113d1438c48907ba9f98c2b83ca
pandaprolp1 <127090383+pandaprolp1@users.noreply.github.com>
2023-11-18 14:13:14 +0300

    Update commands.jsx

--- pages/commands.jsx
+++ pages/commands.jsx
@@ -8,23 +8,18 @@ export default function Commands() {
     "-istatistik",
     "-davet",
     "-küfür-engel",
-    "/setdj",
-    "/play",
-    "/serach",
-    "/stop",
-    "/skip",
-    "/back",
-    "/clear",
-    "/time",
-      "/queue",
-      "/pause",
-      "/resume",
-      "/volume",
-      "/nowplaying",
-      "/save",
-      "/loop",
-      "/filter"
-      ]
+     "-",
+     "-",
+     "-",
+     "-",
+     "-",
+     "-",
+     "-",
+     "-",
+     "-",
+     "-",
+     "-",    
+  ]
   return (
     <>
       <div>
@@ -33,10 +28,10 @@ export default function Commands() {
       </div>
         <p className="text-xl font-medium text-white">
           <i className="fal fa-cogs text-amber-400 mr-2" />
-         Bot Commands
+         Bot Komutları
         </p>
         <p className="text-white text-sm text-opacity-50 mb-5">
-        You can get information about the commands of the Astra Bot.
+       Rias Bot'un komutları hakkında bilgi alabilirsiniz..
         </p>
 
         <div>
@@ -47,7 +42,7 @@ export default function Commands() {
                  
                     <>
                       <i className="fa fa-hashtag text-white text-opacity-20" />
-                     View Commands List
+                    Komut Listesini Görüntüle
                     </>
                 
                 </div>
@@ -85,21 +80,21 @@ export default function Commands() {
   
   <Link href={"https://bit.ly/3PHDjyC"}>
   <a className={"flex items-center px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " }>
-Invite Astra</a>
+Rias'yı davet et</a>
   </Link>
 
   <div className="py-10"></div>
   
   <Link href={"https://discord.gg/ZcDprCrFTn"}>
   <a className={" px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " }>
-Support Server</a>
+Destek Sunucusu</a>
   </Link>
 
   <div className="py-10"></div>
   
   <Link href={"https://bit.ly/3LYzaDe"}>
   <a className={" px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " }>
-Vote Astra</a>
+Rias'ya oy ver</a>
   </Link>
 </div>
 
