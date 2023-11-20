538ff84349d038574acedaf879027277e1d92357
pandaprolp1 <127090383+pandaprolp1@users.noreply.github.com>
2023-11-20 17:40:40 +0300

    Update tos.jsx

--- pages/tos.jsx
+++ pages/tos.jsx
@@ -4,17 +4,18 @@ export default function Tos() {
      <h2 className="mt-4 text-white font-bold text-3xl">Terms of Services</h2>
       <p className="text-sm text-white/30 mb-1">Last Update: 20/05/2022 🆕️</p>
   <div className="animateHeader text-white text-opacity-40 mt-5">
-<p>⊳ Every discord server can use the Astra bot for free, it is completely prohibited to sell or market for a fee.</p>
+<p>⊳ Her discord sunucusu Rias  botunu ücretsiz kullanabilir, ücret karşılığında satışı veya pazarlaması kesinlikle yasaktır.</p>
 <div className="py-3"></div>
-<p>⊳ You cannot use bot commands to harm the bot.</p>
+<p>⊳ Bot komutlarını bota zarar vermek için kullanamazsınız.</p>
 <div className="py-3"></div>
-<p>⊳ You may not use open source codes to harm other servers.</p>
+<p>⊳Açık kaynak kodlarını diğer sunuculara zarar vermek amacıyla kullanamazsınız.</p>
 <div className="py-3"></div>
-<p>⊳ You may not sell or market open source codes through other platforms.</p>
+<p>⊳ Açık kaynak kodlarını diğer platformlar aracılığıyla satamaz veya pazarlayamazsınız..</p>
 <div className="py-3"></div>
-<p>⊳ While using the bot, you cannot use it in a way that harms other users for troll purposes.</p>
+<p>⊳Botu kullanırken troll amaçlı, diğer kullanıcılara zarar verecek şekilde kullanamazsınız.</p>
 <div className="py-3"></div>
-<p>⊳ To use certain discord bot commands, you need to vote for the <a className="text-white" href="https://top.gg/bot/924325576095973426/vote">Astra</a> bot at certain intervals.</p>
+<p>⊳ TBelirli discord bot komutlarını kullanmak için oy vermeniz gerekir. <a className="text-white" href="">Rias Bot
+</a> bot at certain intervals.</p>
 </div>
     </div>
   );
