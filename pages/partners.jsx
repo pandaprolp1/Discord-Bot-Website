25d2c40e2869c71b94a99c0c1926413cd61f4699
pandaprolp1 <127090383+pandaprolp1@users.noreply.github.com>
2023-11-20 17:44:03 +0300

    Update partners.jsx

--- pages/partners.jsx
+++ pages/partners.jsx
@@ -23,7 +23,7 @@ export default function Partners() {
                                 <h1 className="leading-none text-3xl font-bold text-white">Lopper</h1>        
                                 <div className="flex items-center mt-1">
                                     <p className="font-normal font-sm">
-                                       sa!
+                                       sa
                                     </p>
                                 </div>     
                             </div>
